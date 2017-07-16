fa09658ad0141d00c63f58a466aa99133bcc2868
Sandeepkumar Parekh <sandeepparekh9@gmail.com>
2017-07-16 18:42:59 +1000

    added nasa patent json dataset

---
+++ FakeRest/Controllers/NasaPatent.cs
@@ -0,0 +1,10 @@
+﻿using FakeRest.Models;
+using System.Web.Http;
+
+namespace FakeRest.Controllers
+{
+    [RoutePrefix("api/nasapatent")]
+    public class NasaPatentController : BaseController<NasaPatent>
+    {
+    }
+}
---
+++ FakeRest/Models/NasaPatent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakeRest.Models
+{
+    public class NasaPatent : BaseClass
+    {
+        public string Center { get; set; }
+        public string Status { get; set; }
+        public string CaseNumber { get; set; }
+        public string PatentNumber { get; set; }
+        public string ApplicationSn { get; set; }
+        public string Title { get; set; }
+        public string PatentExpirationDate { get; set; }
+        public override string GetSearchableText()
+        {
+            return Center + Status + CaseNumber + PatentNumber + ApplicationSn + Title;
+        }
+    }
+}
