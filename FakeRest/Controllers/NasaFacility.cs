7525b04cfc8df51850ec2a8ad2697a9bca57e9de
Sandeepkumar Parekh <sandeepparekh9@gmail.com>
2017-07-16 15:58:38 +1000

    added nasa facitliy json data from https://data.nasa.gov/Management-Operations/NASA-Facilities/gvk9-iz74

---
+++ FakeRest/Controllers/NasaFacility.cs
@@ -0,0 +1,10 @@
+﻿using FakeRest.Models;
+using System.Web.Http;
+
+namespace FakeRest.Controllers
+{
+    [RoutePrefix("api/nasafacility")]
+    public class NasaFacilityController : BaseController<NasaFacility>
+    {
+    }
+}
---
+++ FakeRest/Models/NasaFacility.cs
@@ -0,0 +1,26 @@
+﻿namespace FakeRest.Models
+{
+    public class NasaFacility : BaseClass
+    {
+        public string Center { get; set; }
+        public string CenterSearchStatus { get; set; }
+        public string Facility { get; set; }
+        public string Occupied { get; set; }
+        public string Status { get; set; }
+        public string UrlLink { get; set; }
+        public string RecordDate { get; set; }
+        public string LastUpdate { get; set; }
+        public string Country { get; set; }
+        public string Contact { get; set; }
+        public string Phone { get; set; }
+        public string Location { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public override string GetSearchableText()
+        {
+            return Center + CenterSearchStatus + Status + Facility + Country + City + State + Zipcode;
+        }
+    }
+
+}
